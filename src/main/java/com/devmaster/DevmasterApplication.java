636f94adf27d273c052732092c365d4141cdbf6f
ranelho <ranelhol@gmail.com>
2025-12-25 09:35:54 -0300

    first commit

---
+++ src/main/java/com/devmaster/DevmasterApplication.java
@@ -0,0 +1,13 @@
+package com.devmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmaster/DevmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
